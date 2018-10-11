a40c1143efb215d800d48698c4b9fd43612c4d11
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2018-10-11 11:02:31 -0700

    Fix bug in CI

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,6 @@
 import sys
 # import sphinx_rtd_theme
 
-from ase import __version__
 
 sys.path.append('.')
 assert sys.version_info >= (2, 7)
@@ -21,18 +20,3 @@ default_role = 'math'
 pygments_style = 'sphinx'
 autoclass_content = 'both'
 modindex_common_prefix = ['bal.']
-
-# html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# html_static_path = ['static']
-# html_last_updated_fmt = '%a, %d %b %Y %H:%M:%S'
-
-
-
-# Avoid GUI windows during doctest:
-doctest_global_setup = """
-import ase.visualize as visualize
-from ase import Atoms
-visualize.view = lambda atoms: None
-Atoms.edit = lambda self: None
-"""
