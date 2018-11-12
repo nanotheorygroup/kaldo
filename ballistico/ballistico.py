ce9799dafbcea0dd67535e1cd5e00332721adaed
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2018-11-12 11:24:51 -0800

    Fix getter and setter with persistency

--- ballistico/ballistico.py
+++ ballistico/ballistico.py
@@ -16,9 +16,10 @@ class Ballistico (Phonons):
         super(self.__class__, self).__init__(atoms=atoms, supercell=supercell, kpts=kpts)
         self.second_order = second_order
         self.third_order = third_order
-        directory = os.path.dirname (self.folder)
-        if not os.path.exists (directory):
-            os.makedirs (directory)
+        folder = type (self).__name__
+        if not os.path.exists (folder):
+            os.makedirs (folder)
+
 
     @property
     def frequencies(self):
@@ -28,8 +29,16 @@ class Ballistico (Phonons):
     def frequencies(self):
         if super (self.__class__, self).frequencies is not None:
             return super (self.__class__, self).frequencies
-        self.calculate_second_all_grid ()
-        return self._frequencies
+        frequencies, eigenvalues, eigenvectors, velocities = ballistico.calculator.calculate_second_all_grid(
+            self.k_size,
+            self.atoms,
+            self.second_order,
+            self.list_of_replicas)
+        self.frequencies = frequencies
+        self.eigenvalues = eigenvalues
+        self.velocities = velocities
+        self.eigenvectors = eigenvectors
+        return frequencies
 
     @frequencies.setter
     def frequencies(self, new_frequencies):
@@ -43,8 +52,16 @@ class Ballistico (Phonons):
     def velocities(self):
         if super (self.__class__, self).velocities is not None:
             return super (self.__class__, self).velocities
-        self.calculate_second_all_grid ()
-        return self._velocities
+        frequencies, eigenvalues, eigenvectors, velocities = ballistico.calculator.calculate_second_all_grid(
+            self.k_size,
+            self.atoms,
+            self.second_order,
+            self.list_of_replicas)
+        self.frequencies = frequencies
+        self.eigenvalues = eigenvalues
+        self.velocities = velocities
+        self.eigenvectors = eigenvectors
+        return velocities
 
     @velocities.setter
     def velocities(self, new_velocities):
@@ -58,8 +75,16 @@ class Ballistico (Phonons):
     def eigenvalues(self):
         if super (self.__class__, self).eigenvalues is not None:
             return super (self.__class__, self).eigenvalues
-        self.calculate_second_all_grid ()
-        return self._eigenvalues
+        frequencies, eigenvalues, eigenvectors, velocities = ballistico.calculator.calculate_second_all_grid(
+            self.k_size,
+            self.atoms,
+            self.second_order,
+            self.list_of_replicas)
+        self.frequencies = frequencies
+        self.eigenvalues = eigenvalues
+        self.velocities = velocities
+        self.eigenvectors = eigenvectors
+        return eigenvalues
 
     @eigenvalues.setter
     def eigenvalues(self, new_eigenvalues):
@@ -77,9 +102,16 @@ class Ballistico (Phonons):
     def eigenvectors(self):
         if super (self.__class__, self).eigenvectors is not None:
             return super (self.__class__, self).eigenvectors
-        else:
-            self.calculate_second_all_grid ()
-        return self._eigenvectors
+        frequencies, eigenvalues, eigenvectors, velocities = ballistico.calculator.calculate_second_all_grid(
+            self.k_size,
+            self.atoms,
+            self.second_order,
+            self.list_of_replicas)
+        self.frequencies = frequencies
+        self.eigenvalues = eigenvalues
+        self.velocities = velocities
+        self.eigenvectors = eigenvectors
+        return eigenvectors
 
     @property
     def dos(self):
@@ -93,8 +125,14 @@ class Ballistico (Phonons):
     def dos(self):
         if super (self.__class__, self).dos is not None:
             return super (self.__class__, self).dos
-        self.calculate_dos ()
-        return self._dos
+        # TODO: delta needs to be set by the instance
+        delta = 1
+        dos = ballistico.calculator.calculate_density_of_states(
+            self.frequencies,
+            self.k_size,
+            delta)
+        self.dos = dos
+        return dos
 
     @property
     def occupations(self):
@@ -108,8 +146,9 @@ class Ballistico (Phonons):
     def occupations(self):
         if super (self.__class__, self).occupations is not None:
             return super (self.__class__, self).occupations
-        self.calculate_occupations ()
-        return self._occupations
+        occupations = ballistico.calculator.calculate_occupations(self.frequencies, self.temperature, self.is_classic)
+        self.occupations = occupations
+        return occupations
 
     @property
     def gamma(self):
@@ -123,32 +162,8 @@ class Ballistico (Phonons):
     def gamma(self):
         if super (self.__class__, self).gamma is not None:
             return super (self.__class__, self).gamma
-        self.calculate_gamma ()
-        return self._gamma
-
-    def calculate_dos(self, delta=1):
-        self.dos = ballistico.calculator.calculate_density_of_states(
-            self.frequencies,
-            self.k_size,
-            delta)
-
-    # @profile
-    def calculate_second_all_grid(self):
-        frequencies, eigenvalues, eigenvectors, velocities = ballistico.calculator.calculate_second_all_grid(
-            self.k_size,
-            self.atoms,
-            self.second_order,
-            self.list_of_replicas)
-        self.frequencies = frequencies
-        self.eigenvalues = eigenvalues
-        self.velocities = velocities
-        self.eigenvectors = eigenvectors
-    
-    def calculate_occupations(self):
-        self.occupations = ballistico.calculator.calculate_occupations(self.frequencies, self.temperature, self.is_classic)
-
-    def calculate_gamma(self, sigma_in=None):
-        self.gamma = ballistico.calculator.calculate_gamma(
+        #TODO: Find a way to pass sigma in when needed
+        gamma = ballistico.calculator.calculate_gamma(
             self.atoms,
             self.frequencies,
             self.velocities,
@@ -156,5 +171,9 @@ class Ballistico (Phonons):
             self.k_size,
             self.eigenvectors,
             self.list_of_replicas,
-            self.third_order,
-            sigma_in)
+            self.third_order)
+        self.gamma = gamma
+        return gamma
+    
+    
+    
--- ballistico/phonons.py
+++ ballistico/phonons.py
@@ -1,14 +1,15 @@
 import numpy as np
 import ballistico.atoms_helper as atom_helper
 
-FREQUENCY_K_FILE = 'frequencies.npy'
-
+FREQUENCIES_FILE = 'frequencies.npy'
 EIGENVALUES_FILE = 'eigenvalues.npy'
 EIGENVECTORS_FILE = 'eigenvectors.npy'
-VELOCITY_K_FILE = 'velocities.npy'
-GAMMA_FILE = 'gammas.npy'
+VELOCITIES_FILE = 'velocities.npy'
+GAMMA_FILE = 'gamma.npy'
+DOS_FILE = 'dos.npy'
+OCCUPATIONS_FILE = 'occupations.npy'
 
-IS_PERSISTENCY_ENABLED = False
+IS_PERSISTENCY_ENABLED = True
 
 class Phonons (object):
 	def __init__(self, atoms, supercell = (1, 1, 1), kpts = (1, 1, 1), is_classic = False, temperature = 300):
@@ -16,7 +17,6 @@ class Phonons (object):
 		self.supercell = np.array (supercell)
 		self.k_size = np.array (kpts)
 		self.is_classic = is_classic
-		self.folder = 'phonons/'
 		[self.replicated_atoms, self.list_of_replicas] = \
 			atom_helper.replicate_atoms (self.atoms, self.supercell)
 		
@@ -44,7 +44,9 @@ class Phonons (object):
 	def frequencies(self):
 		if self._frequencies is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._frequencies = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._frequencies = np.load (folder + FREQUENCIES_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._frequencies
@@ -53,7 +55,9 @@ class Phonons (object):
 	@frequencies.setter
 	def frequencies(self, new_frequencies):
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._frequencies)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + FREQUENCIES_FILE, new_frequencies)
 		self._frequencies = new_frequencies
 	
 	@property
@@ -64,7 +68,9 @@ class Phonons (object):
 	def velocities(self):
 		if self._velocities is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._velocities = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._velocities = np.load (folder + VELOCITIES_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._velocities
@@ -72,7 +78,9 @@ class Phonons (object):
 	@velocities.setter
 	def velocities(self, new_velocities):
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._velocities)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + VELOCITIES_FILE, new_velocities)
 		self._velocities = new_velocities
 	
 	@property
@@ -83,7 +91,9 @@ class Phonons (object):
 	def eigenvectors(self):
 		if self._eigenvectors is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._eigenvectors = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._eigenvectors = np.load (folder + EIGENVECTORS_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._eigenvectors
@@ -91,7 +101,9 @@ class Phonons (object):
 	@eigenvectors.setter
 	def eigenvectors(self, new_eigenvectors):
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._eigenvectors)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + EIGENVECTORS_FILE, new_eigenvectors)
 		self._eigenvectors = new_eigenvectors
 	
 	@property
@@ -102,7 +114,9 @@ class Phonons (object):
 	def eigenvalues(self):
 		if self._eigenvalues is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._eigenvalues = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._eigenvalues = np.load (folder + EIGENVALUES_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._eigenvalues
@@ -110,7 +124,9 @@ class Phonons (object):
 	@eigenvalues.setter
 	def eigenvalues(self, new_eigenvalues):
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._eigenvalues)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + EIGENVALUES_FILE, new_eigenvalues)
 		self._eigenvalues = new_eigenvalues
 	
 	@property
@@ -119,17 +135,23 @@ class Phonons (object):
 	
 	@gamma.getter
 	def gamma(self):
+		#TODO separate gamma classic and quantum
 		if self._gamma is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._gamma = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._gamma = np.load (folder + GAMMA_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._gamma
 	
 	@gamma.setter
 	def gamma(self, new_gamma):
+		#TODO separate gamma classic and quantum
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._gamma)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + GAMMA_FILE, new_gamma)
 		self._gamma = new_gamma
 	
 	@property
@@ -140,7 +162,9 @@ class Phonons (object):
 	def dos(self):
 		if self._dos is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._dos = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._dos = np.load (DOS_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._dos
@@ -148,7 +172,9 @@ class Phonons (object):
 	@dos.setter
 	def dos(self, new_dos):
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._dos)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + DOS_FILE, new_dos)
 		self._dos = new_dos
 	
 	@property
@@ -157,15 +183,21 @@ class Phonons (object):
 	
 	@occupations.getter
 	def occupations(self):
+		#TODO:add a temperature subfolder here
 		if self._occupations is None and IS_PERSISTENCY_ENABLED:
 			try:
-				self._occupations = np.load (self.folder + FREQUENCY_K_FILE)
+				folder = type(self).__name__
+				folder += '/'
+				self._occupations = np.load (folder + OCCUPATIONS_FILE)
 			except FileNotFoundError as e:
 				print (e)
 		return self._occupations
 	
 	@occupations.setter
 	def occupations(self, new_occupations):
+		#TODO:add a temperature subfolder here
 		if IS_PERSISTENCY_ENABLED:
-			np.save (self.folder + FREQUENCY_K_FILE, self._occupations)
+			folder = type (self).__name__
+			folder += '/'
+			np.save (folder + OCCUPATIONS_FILE, new_occupations)
 		self._occupations = new_occupations
