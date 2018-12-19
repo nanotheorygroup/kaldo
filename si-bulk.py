f9ff26c8da564eaae3f9d8d590e7757a04ea4638
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2018-12-19 11:20:43 -0800

    Improve main

--- si-bulk.py
+++ si-bulk.py
@@ -22,66 +22,53 @@ if __name__ == "__main__":
     # and replicate it
     supercell = np.array([3, 3, 3])
     n_replicas = np.prod(supercell)
-    temperatures = np.array([2, 10, 40, 100, 200, 300, 500, 1000])
-    conductivities = np.zeros_like(temperatures, dtype=np.float)
-    heat_capacities = np.zeros_like(conductivities)
 
-    for i in range(temperatures.shape[0]):
-        temperature = temperatures[i]
-        # we create our system
-        # temperature = 300
+    temperature = 300
+    # we create our system
+    # temperature = 300
 
-        # our Phonons object built on the system
-        kpts = np.array([7, 7, 7])
+    # our Phonons object built on the system
+    kpts = np.array([7, 7, 7])
 
-        calculator = LAMMPSlib
-        calculator_inputs = {'lmpcmds': ["pair_style tersoff", "pair_coeff * * forcefields/Si.tersoff Si"],
-                             'log_file': 'log_lammps.out'}
+    calculator = LAMMPSlib
+    calculator_inputs = {'lmpcmds': ["pair_style tersoff", "pair_coeff * * forcefields/Si.tersoff Si"],
+                         'log_file': 'log_lammps.out'}
 
-        # calculator = Espresso
-        # calculator_inputs = {'pseudopotentials':{'Si': 'Si.pz-n-kjpaw_psl.0.1.UPF'},
-        #                 'tstress':True,
-        #                 'tprnfor':True,
-        #                 'input_data':
-        #                      {'system': {'ecutwfc': 16.0},
-        #                                            'electrons': {'conv_thr': 1e-8},
-        #                                            'disk_io': 'low',
-        #                                            'pseudo_dir': '/home/giuseppe/espresso/pseudo/'},
-        #                 'koffset':(2, 2, 2),
-        #                 'kpoints':(1, 1, 1)}
+    # calculator = Espresso
+    # calculator_inputs = {'pseudopotentials':{'Si': 'Si.pz-n-kjpaw_psl.0.1.UPF'},
+    #                 'tstress':True,
+    #                 'tprnfor':True,
+    #                 'input_data':
+    #                      {'system': {'ecutwfc': 16.0},
+    #                                            'electrons': {'conv_thr': 1e-8},
+    #                                            'disk_io': 'low',
+    #                                            'pseudo_dir': '/home/giuseppe/espresso/pseudo/'},
+    #                 'koffset':(2, 2, 2),
+    #                 'kpoints':(1, 1, 1)}
 
-        third_order_symmerty_inputs = {'NNEIGH': 4, 'SYMPREC': 1e-5}
+    third_order_symmerty_inputs = {'NNEIGH': 4, 'SYMPREC': 1e-5}
 
-        # Create a finite difference object
-        finite_difference = FiniteDifference(atoms=atoms,
-                                             supercell=supercell,
-                                             calculator=calculator,
-                                             calculator_inputs=calculator_inputs,
-                                             is_persistency_enabled=True)
+    # Create a finite difference object
+    finite_difference = FiniteDifference(atoms=atoms,
+                                         supercell=supercell,
+                                         calculator=calculator,
+                                         calculator_inputs=calculator_inputs,
+                                         is_persistency_enabled=False)
 
-        # Create a phonon object
-        phonons = Phonons(finite_difference=finite_difference, kpts=kpts, is_classic=is_classic,
-                          temperature=temperature, is_persistency_enabled=True)
-        # Create a plot helper object
-        # plotter = Plotter (phonons=phonons,
-        #                    is_showing=True,
-        #                    folder='plot/ballistico/',
-        #                    is_persistency_enabled=True)
+    # Create a phonon object
+    phonons = Phonons(finite_difference=finite_difference, kpts=kpts, is_classic=is_classic,
+                      temperature=temperature, is_persistency_enabled=False)
+    # Create a plot helper object
+    plotter = Plotter (phonons=phonons,
+                       is_showing=True,
+                       folder='plot/ballistico/',
+                       is_persistency_enabled=True)
 
-        # call the method plot everything
-        # plotter.plot_everything()
+    # call the method plot everything
+    plotter.plot_everything()
 
-        # calculate the conductivity creating a conductivity object and calling the
-        # calculate_conductivity method
-        heat_capacities[i] = phonons.c_v.mean()
-        conductivities[i] = ConductivityController(phonons).calculate_conductivity(is_classical=is_classic)[0, 0]
-
-    plt.plot(temperatures, conductivities)
-    plt.show()
-
-    print(conductivities)
-
-    plt.plot(temperatures, heat_capacities)
-    plt.show()
-
-    print(heat_capacities)
+    # calculate the conductivity creating a conductivity object and calling the
+    # calculate_conductivity method
+    # heat_capacity = phonons.c_v.mean()
+    conductivity = ConductivityController(phonons).calculate_conductivity(is_classical=is_classic)[0, 0]
+    print(conductivity)
