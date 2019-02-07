7524411d0806f80265eda005f820029985c5e443
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-02-07 15:45:56 -0800

    Update dependencies, change project name

--- setup.py
+++ setup.py
@@ -3,19 +3,32 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="ballistico",
-    version="0.0.1",
-    author="Giuseppe Barbalinardo",
-    author_email="giuseppe.barbalinardo@gmail.com",
-    description="Ballistico Package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://gitlab.com/gbarbalinardo/ballistico-new",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='ballistico',
+        version="0.0.1",
+        description='Anharmonic Lattice Dynamics calculator from ASE objects',
+        long_description=long_description,
+        author='Giuseppe Barbalinardo',
+        author_email='giuseppe.barbalinardo@gmail.com',
+        url="https://gitlab.com/gbarbalinardo/finitedifference",
+        license='BSD-3C',
+        packages = [
+            'finitedifference'
+            ],
+        package_dir = {
+            'finitedifference': 'finitedifference'
+            },
+        install_requires=[
+            'numpy>=1.7',
+            'scipy>=1',
+            'ase>=3.16.0',
+            'sparse>=0.6',
+            'pandas>=0.23',
+            'spglib>=1.11',
+            'opt_einsum>=2.3',
+            'seaborn>=0.9',
+        ],
+        zip_safe=True
+    )
