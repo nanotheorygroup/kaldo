f56616b6c3ffef56c8e4efbcd5e125f91429ae30
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-05-07 13:27:06 -0700

    Replace tf einsum

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
             'sparse>=0.6',
             'spglib>=1.11',
             'opt_einsum>=2.3',
-            'seekpath>=1.8'
+            'seekpath>=1.8',
+            'tensorflow>=1.13'
             ],
         zip_safe=True
     )
