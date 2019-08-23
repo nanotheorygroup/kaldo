1770a60e31a2a3beb0ee339426d00a0a3db850e7
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-08-23 06:50:15 -0700

    Start cleanup of conductivity controller

--- ballistico/conductivitycontroller.py
+++ ballistico/conductivitycontroller.py
@@ -7,8 +7,28 @@ MAX_ITERATIONS_SC = 500
 class ConductivityController:
     def __init__(self, phonons):
         self.phonons = phonons
+        frequencies = self.phonons.frequencies.reshape((self.phonons.n_k_points * self.phonons.n_modes), order='C')
+        physical_modes = (frequencies > self.phonons.frequency_threshold)
+        gamma = self.phonons.gamma.reshape((self.phonons.n_phonons), order='C')
 
-    def conductivity(self, mfp):
+        gamma_physical = np.zeros_like(gamma)
+        gamma_physical[physical_modes] = gamma[physical_modes]
+        self.tau = np.zeros_like(gamma)
+        self.tau[physical_modes] = 1 / gamma_physical[physical_modes]
+
+        # TODO: move this minus sign somewhere else
+        self.gamma_tensor = - 1 * self.phonons.gamma_tensor
+        self.gamma_tensor = contract('a,ab,b->ab', 1 / frequencies, self.gamma_tensor, frequencies)
+        scattering_matrix = np.diag(gamma_physical) + self.gamma_tensor
+
+        # Let's remove the unphysical modes from the matrix
+        index = np.outer(physical_modes, physical_modes)
+        self.scattering_matrix = scattering_matrix[index].reshape((physical_modes.sum(), physical_modes.sum()), order='C')
+
+
+
+    def conductivity(self, lambd):
+
         # TODO: Change units conversion in this method
         volume = np.linalg.det(self.phonons.atoms.cell) / 1000
         frequencies = self.phonons.frequencies.reshape((self.phonons.n_phonons), order='C')
@@ -17,33 +37,20 @@ class ConductivityController:
         velocities = self.phonons.velocities.real.reshape((self.phonons.n_phonons, 3), order='C') / 10
         conductivity_per_mode = np.zeros((self.phonons.n_phonons, 3, 3))
         conductivity_per_mode[physical_modes, :, :] = 1 / (volume * self.phonons.n_k_points) * c_v[physical_modes, np.newaxis, np.newaxis] * \
-                                                      velocities[physical_modes, :, np.newaxis] * mfp[physical_modes,np.newaxis, :]
+                                                      velocities[physical_modes, :, np.newaxis] * lambd[physical_modes, np.newaxis, :]
         return conductivity_per_mode
 
     def calculate_conductivity_inverse(self):
-
-        scattering_matrix = self.phonons.gamma_tensor
-
         velocities = self.phonons.velocities.real.reshape((self.phonons.n_phonons, 3), order='C') / 10
         frequencies = self.phonons.frequencies.reshape((self.phonons.n_k_points * self.phonons.n_modes), order='C')
-        physical_modes = (frequencies > self.phonons.frequency_threshold)  # & (velocities > 0)[:, 2]
-        gamma = self.phonons.gamma.reshape((self.phonons.n_phonons), order='C')
-        a_in = - 1 * scattering_matrix.reshape((self.phonons.n_phonons, self.phonons.n_phonons), order='C')
-        a_in = contract('a,ab,b->ab', 1 / frequencies, a_in, frequencies)
-        a_out = np.zeros_like(gamma)
-        a_out[physical_modes] = gamma[physical_modes]
-        a_out_inverse = np.zeros_like(gamma)
-        a_out_inverse[physical_modes] = 1 / a_out[physical_modes]
-        a = np.diag(a_out) + a_in
-
-        # Let's remove the unphysical modes from the matrix
-        index = np.outer(physical_modes, physical_modes)
-        a = a[index].reshape((physical_modes.sum(), physical_modes.sum()), order='C')
-        a_inverse = np.linalg.inv(a)
+        physical_modes = (frequencies > self.phonons.frequency_threshold)
+        scattering_inverse = np.linalg.inv(self.scattering_matrix)
         lambd = np.zeros((self.phonons.n_phonons, 3))
-        lambd[physical_modes, :] = a_inverse.dot(velocities[physical_modes, :])
+        lambd[physical_modes, :] = scattering_inverse.dot(velocities[physical_modes, :])
         conductivity_per_mode = self.conductivity(lambd)
-        evals = np.linalg.eigvalsh(a)
+
+        #TODO: remove this debug info
+        evals = np.linalg.eigvalsh(scattering_inverse)
         print('negative eigenvals : ', (evals < 0).sum())
         return conductivity_per_mode
 
@@ -64,32 +71,22 @@ class ConductivityController:
         physical_modes = (frequencies > self.phonons.frequency_threshold)
 
         velocities = self.phonons.velocities.real.reshape((self.phonons.n_phonons, 3), order='C') / 10
-        physical_modes = physical_modes  # & (velocities > 0)[:, 2]
 
-        gamma = self.phonons.gamma.reshape((self.phonons.n_phonons), order='C')
-        scattering_matrix = self.phonons.gamma_tensor
 
-        a_in = - 1 * scattering_matrix.reshape((self.phonons.n_phonons, self.phonons.n_phonons), order='C')
-        a_in = contract('a,ab,b->ab', 1 / frequencies, a_in, frequencies)
-        a_out = np.zeros_like(gamma)
-        a_out[physical_modes] = gamma[physical_modes]
-        a_out_inverse = np.zeros_like(gamma)
-        a_out_inverse[physical_modes] = 1 / a_out[physical_modes]
-        a = np.diag(a_out) + a_in
-        b = a_out_inverse[:, np.newaxis] * velocities[:, :]
-        a_out_inverse_a_in_to_n_times_b = np.copy(b)
-        f_n = np.copy(b)
+        lambda_0 = self.tau[:, np.newaxis] * velocities[:, :]
+        delta_lambda = np.copy(lambda_0)
+        lambda_n = np.copy(lambda_0)
         conductivity_value = np.zeros((3, 3, max_n_iterations))
 
         for n_iteration in range(max_n_iterations):
 
-            a_out_inverse_a_in_to_n_times_b[:, :] = -1 * (a_out_inverse[:, np.newaxis] * a_in[:, physical_modes]).dot(
-                a_out_inverse_a_in_to_n_times_b[physical_modes, :])
-            f_n += a_out_inverse_a_in_to_n_times_b
+            delta_lambda[:, :] = -1 * (self.tau[:, np.newaxis] * self.gamma_tensor[:, physical_modes]).dot(
+                delta_lambda[physical_modes, :])
+            lambda_n += delta_lambda
 
-            conductivity_value[:, :, n_iteration] = self.conductivity(f_n).sum(0)
+            conductivity_value[:, :, n_iteration] = self.conductivity(lambda_n).sum(0)
 
-        conductivity_per_mode = self.conductivity(f_n)
+        conductivity_per_mode = self.conductivity(lambda_n)
         if n_iteration == (max_n_iterations - 1):
             print('Max iterations reached')
         return conductivity_per_mode, conductivity_value
