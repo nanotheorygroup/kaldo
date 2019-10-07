Fix bug in gamma calculation with TF

ballistico/anharmonic_tf.py
@@ -81,7 +81,7 @@ def calculate_dirac_delta_amorphous(phonons, mu):
             if is_plus:
                 dirac_delta_tf = tf.gather(density_tf[0], mup_vec) - tf.gather(density_tf[0], mupp_vec)
             else:
-                dirac_delta_tf = 1 + tf.gather(density_tf[0], mup_vec) + tf.gather(density_tf[0], mupp_vec)
+                dirac_delta_tf = 0.5 * (1 + tf.gather(density_tf[0], mup_vec) + tf.gather(density_tf[0], mupp_vec))
             dirac_delta_tf = dirac_delta_tf / tf.gather(omega_tf[0], mup_vec) / tf.gather(omega_tf[0], mupp_vec)
             omegas_difference_tf = tf.abs(phonons._omegas[0, mu] + second_sign * tf.gather(omega_tf[0], mup_vec) - tf.gather(omega_tf[0],
                                                                                                                 mupp_vec))

ballistico/tests/test_amorphous.py
@@ -7,7 +7,6 @@ from finitedifference.finitedifference import FiniteDifference
 import numpy as np
 from ballistico.phonons import Phonons
 import ballistico.conductivity as bac
-import os
 import shutil
 
 TMP_FOLDER = 'tmp-folder'
@@ -23,7 +22,6 @@ def create_phonons():
                       folder=TMP_FOLDER,
                       sigma_in= 0.05 / 4.135,
                       broadening_shape='triangle')
-
     return phonons
 
 
@@ -32,5 +30,6 @@ def test_qhgk_conductivity():
     phonons = create_phonons()
     cond = bac.conductivity(phonons, method='qhgk').sum(axis=0)
     cond = np.abs(np.mean(cond.diagonal()))
-    np.testing.assert_approx_equal(cond, 0.971, significant=3)
+    print(cond)
+    np.testing.assert_approx_equal(cond, 0.99, significant=2)
 