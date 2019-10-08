bf27558d624b0646f69b8b37d62b46b2765312d1
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-10-08 07:23:11 -0700

    Revert "Add temporarily folder for unit tests"
    This reverts commit f58614f5fec084f5ad0193c1402861f697ce15c7.

--- ballistico/tests/test_amorphous.py
+++ ballistico/tests/test_amorphous.py
@@ -9,9 +9,12 @@ from ballistico.phonons import Phonons
 import ballistico.conductivity as bac
 import ase.units as units
 import shutil
+TMP_FOLDER = 'ballistico/tests/tmp-folder'
 
 
-def create_phonons(tmpdir):
+def create_phonons():
+    shutil.rmtree(TMP_FOLDER, ignore_errors=True)
+
     # Create a finite difference object
     finite_difference = FiniteDifference.import_from_dlpoly_folder(folder='ballistico/tests/si-amorphous')
 
@@ -19,26 +22,26 @@ def create_phonons(tmpdir):
     phonons = Phonons(finite_difference=finite_difference,
                       is_classic=True,
                       temperature=300,
-                      folder=tmpdir,
+                      folder=TMP_FOLDER,
                       sigma_in= 0.05 / 4.135,
                       broadening_shape='triangle')
     return phonons
 
 
-def test_first_gamma(tmpdir):
-    phonons = create_phonons(tmpdir)
+def test_first_gamma():
+    phonons = create_phonons()
     THZTOMEV = units.J * units._hbar * 2 * np.pi * 1e15
     np.testing.assert_approx_equal(phonons.gamma[3] * THZTOMEV / (2 * np.pi), 22.451, significant=3)
 
 
-def test_second_gamma(tmpdir):
-    phonons = create_phonons(tmpdir)
+def test_second_gamma():
+    phonons = create_phonons()
     THZTOMEV = units.J * units._hbar * 2 * np.pi * 1e15
     np.testing.assert_approx_equal(phonons.gamma[4] * THZTOMEV / (2 * np.pi), 23.980, significant=3)
 
 
-def test_qhgk_conductivity(tmpdir):
-    phonons = create_phonons(tmpdir)
+def test_qhgk_conductivity():
+    phonons = create_phonons()
     cond = bac.conductivity(phonons, method='qhgk').sum(axis=0)
     cond = np.abs(np.mean(cond.diagonal()))
     np.testing.assert_approx_equal(cond, 0.99, significant=2)
