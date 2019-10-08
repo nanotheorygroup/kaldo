f58614f5fec084f5ad0193c1402861f697ce15c7
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-10-08 07:02:50 -0700

    Add temporarily folder for unit tests

--- ballistico/tests/test_crystal.py
+++ ballistico/tests/test_crystal.py
@@ -9,9 +9,8 @@ from ballistico.phonons import Phonons
 import ballistico.conductivity as bac
 import shutil
 
-TMP_FOLDER = 'ballistico/tests/tmp-folder'
 
-def create_phonons():
+def create_phonons(tmpdir):
     # Create a finite difference object
     finite_difference = FiniteDifference.import_from_dlpoly_folder(folder='ballistico/tests/si-crystal',
                                                                    supercell=[3, 3, 3])
@@ -21,12 +20,11 @@ def create_phonons():
                       kpts=[5, 5, 5],
                       is_classic=False,
                       temperature=300,
-                      folder=TMP_FOLDER)
+                      folder=tmpdir)
     return phonons
 
 
 def test_qhgk_conductivity():
-    shutil.rmtree(TMP_FOLDER, ignore_errors=True)
     phonons = create_phonons()
     cond = bac.conductivity(phonons, method='qhgk').sum(axis=0)
     cond = np.abs(np.mean(cond.diagonal()))
