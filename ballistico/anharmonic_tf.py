368db58cd0c1d2ad4aa813e0748547386c05253d
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-11-05 17:46:44 -0800

    Clean up anharmonic controllers

--- ballistico/anharmonic_tf.py
+++ ballistico/anharmonic_tf.py
@@ -5,112 +5,17 @@ Anharmonic Lattice Dynamics
 import numpy as np
 from opt_einsum import contract
 import ase.units as units
-from .tools import timeit, lazy_property, is_calculated
+from .tools import timeit
 import tensorflow as tf
 
 DELTA_THRESHOLD = 2
-IS_DELTA_CORRECTION_ENABLED = False
 EVTOTENJOVERMOL = units.mol / (10 * units.J)
 KELVINTOJOULE = units.kB / units.J
 KELVINTOTHZ = units.kB / units.J / (2 * np.pi * units._hbar) * 1e-12
 GAMMATOTHZ = 1e11 * units.mol * EVTOTENJOVERMOL ** 2
 
 
-
-def calculate_dirac_delta_crystal(phonons, index_kpp_full, index_k, mu, is_plus):
-    if phonons.frequencies[index_k, mu] <= phonons.frequency_threshold:
-        return None
-    if phonons.sigma_in:
-        sigma_tf = tf.constant(phonons.sigma_in, dtype=tf.float64)
-    else:
-        sigma_tf = calculate_broadening(phonons, index_kpp_full)
-    second_sign = (int(is_plus) * 2 - 1)
-    omegas_difference = tf.abs(phonons._omega_tf[index_k, mu] + second_sign * phonons._omega_tf[:, :, tf.newaxis] -
-                               tf.gather(phonons._omega_tf, index_kpp_full)[:, tf.newaxis, :])
-    condition = (omegas_difference < DELTA_THRESHOLD * 2 * np.pi * sigma_tf) & \
-                (phonons._frequencies_tf[:, :, tf.newaxis] > phonons.frequency_threshold) & \
-                (tf.gather(phonons._frequencies_tf, index_kpp_full)[:, tf.newaxis, :] > phonons.frequency_threshold)
-    interactions = tf.where(condition)
-    if interactions.shape[0] > 0:
-        index_kp_vec = tf.cast(interactions[:, 0], dtype=tf.int32)
-        # kp_vec = tf.unravel_index(index_kp_vec, phonons.kpts)
-        # kpp_vec = k[:, tf.newaxis] + (int(is_plus) * 2 - 1) * tf.cast(kp_vec[:, :], dtype=tf.int32)
-        # kpp_vec = tf.math.floormod(kpp_vec, phonons.kpts[:, tf.newaxis])
-        # index_kpp_vec = tf.cast(phonons.kpts[2] * phonons.kpts[1] * kpp_vec[0, :] + phonons.kpts[2] * kpp_vec[1, :] + kpp_vec[2, :], dtype=tf.int32)
-        index_kpp_vec = tf.gather(index_kpp_full, index_kp_vec)
-        mup_vec = tf.cast(interactions[:, 1], dtype=tf.int32)
-        mupp_vec = tf.cast(interactions[:, 2], dtype=tf.int32)
-        coords_1 = tf.stack((index_kp_vec, mup_vec), axis=-1)
-        coords_2 = tf.stack((index_kpp_vec, mupp_vec), axis=-1)
-        if sigma_tf.shape != []:
-            coords_3 = tf.stack((index_kp_vec, mup_vec, mupp_vec), axis=-1)
-            sigma_tf = tf.gather_nd(sigma_tf, coords_3)
-        if is_plus:
-            dirac_delta_tf = tf.gather_nd(phonons._occupations_tf, coords_1) - tf.gather_nd(phonons._occupations_tf, coords_2)
-
-        else:
-            dirac_delta_tf = 0.5 * (1 + tf.gather_nd(phonons._occupations_tf, coords_1) + tf.gather_nd(phonons._occupations_tf, coords_2))
-        dirac_delta_tf /= (tf.gather_nd(phonons._omega_tf, coords_1) * tf.gather_nd(phonons._omega_tf, coords_2))
-        dirac_delta_tf = dirac_delta_tf * 1 / tf.sqrt(np.pi * (2 * np.pi * sigma_tf) ** 2) * tf.exp(
-            - (phonons._omega_tf[index_k, mu] + second_sign * tf.gather_nd(phonons._omega_tf, coords_1) - tf.gather_nd(phonons._omega_tf, coords_2)) ** 2 / ((2 * np.pi * sigma_tf) ** 2))
-        index_kp = index_kp_vec
-        mup = mup_vec
-        index_kpp = index_kpp_vec
-        mupp = mupp_vec
-        return dirac_delta_tf, index_kp, mup, index_kpp, mupp
-
-def calculate_dirac_delta_amorphous(phonons, mu):
-    if phonons.frequencies[0, mu] < phonons.frequency_threshold:
-        return None
-    if phonons.broadening_shape == 'triangle':
-        delta_threshold = 1
-    else:
-        delta_threshold = DELTA_THRESHOLD
-    density_tf = phonons._occupations_tf
-    omega_tf = phonons._omega_tf
-    sigma_tf = tf.constant(phonons.sigma_in, dtype=tf.float64)
-    for is_plus in (1, 0):
-        second_sign = (int(is_plus) * 2 - 1)
-        omegas_difference = np.abs(phonons._omegas[0, mu] + second_sign * phonons._omegas[0, :, np.newaxis] -
-                                   phonons._omegas[0, np.newaxis, :])
-        condition = (omegas_difference < delta_threshold * 2 * np.pi * sigma_tf) & \
-                    (phonons.frequencies[0, :, np.newaxis] > phonons.frequency_threshold) & \
-                    (phonons.frequencies[0, np.newaxis, :] > phonons.frequency_threshold)
-        interactions = tf.where(condition)
-        if interactions.shape[0] > 0:
-            # Create sparse index
-            mup_vec = interactions[:, 0]
-            mupp_vec = interactions[:, 1]
-            if is_plus:
-                dirac_delta_tf = tf.gather(density_tf[0], mup_vec) - tf.gather(density_tf[0], mupp_vec)
-            else:
-                dirac_delta_tf = 0.5 * (1 + tf.gather(density_tf[0], mup_vec) + tf.gather(density_tf[0], mupp_vec))
-            dirac_delta_tf = dirac_delta_tf / tf.gather(omega_tf[0], mup_vec) / tf.gather(omega_tf[0], mupp_vec)
-            omegas_difference_tf = tf.abs(phonons._omegas[0, mu] + second_sign * tf.gather(omega_tf[0], mup_vec) - tf.gather(omega_tf[0],
-                                                                                                                mupp_vec))
-
-            if phonons.broadening_shape == 'gauss':
-                dirac_delta_tf = dirac_delta_tf * 1 / tf.sqrt(np.pi * (2 * np.pi * sigma_tf) ** 2) * np.exp(
-                    - omegas_difference_tf ** 2 / ((2 * np.pi * sigma_tf) ** 2))
-            elif phonons.broadening_shape == 'triangle':
-                dirac_delta_tf = dirac_delta_tf * 1. / (2 * np.pi * sigma_tf) * (1 - omegas_difference_tf / (2 * np.pi * sigma_tf))
-            else:
-                raise TypeError('Broadening function not implemented.')
-
-            try:
-                mup = tf.concat([mup, mup_vec], 0)
-                mupp = tf.concat([mupp, mupp_vec], 0)
-                current_delta = tf.concat([current_delta, dirac_delta_tf], 0)
-            except NameError:
-                mup = mup_vec
-                mupp = mupp_vec
-                current_delta = dirac_delta_tf
-    try:
-        return current_delta, mup, mupp
-    except:
-        return None
-
-
+@timeit
 def project_amorphous(phonons, is_gamma_tensor_enabled=False):
     if is_gamma_tensor_enabled == True:
         raise ValueError('is_gamma_tensor_enabled=True not supported')
@@ -166,7 +71,7 @@ def project_amorphous(phonons, is_gamma_tensor_enabled=False):
 
     return ps_and_gamma
 
-
+@timeit
 def project_crystal(phonons, is_gamma_tensor_enabled=False):
 
     n_particles = phonons.atoms.positions.shape[0]
@@ -270,6 +175,100 @@ def project_crystal(phonons, is_gamma_tensor_enabled=False):
             ps_and_gamma[nu_single, 1] += tf.reduce_sum(pot_times_dirac)
         ps_and_gamma[nu_single, 1:] /= phonons.frequencies.flatten()[nu_single]
     return ps_and_gamma
+
+
+def calculate_dirac_delta_crystal(phonons, index_kpp_full, index_k, mu, is_plus):
+    if phonons.frequencies[index_k, mu] <= phonons.frequency_threshold:
+        return None
+    if phonons.sigma_in:
+        sigma_tf = tf.constant(phonons.sigma_in, dtype=tf.float64)
+    else:
+        sigma_tf = calculate_broadening(phonons, index_kpp_full)
+    second_sign = (int(is_plus) * 2 - 1)
+    omegas_difference = tf.abs(phonons._omega_tf[index_k, mu] + second_sign * phonons._omega_tf[:, :, tf.newaxis] -
+                               tf.gather(phonons._omega_tf, index_kpp_full)[:, tf.newaxis, :])
+    condition = (omegas_difference < DELTA_THRESHOLD * 2 * np.pi * sigma_tf) & \
+                (phonons._frequencies_tf[:, :, tf.newaxis] > phonons.frequency_threshold) & \
+                (tf.gather(phonons._frequencies_tf, index_kpp_full)[:, tf.newaxis, :] > phonons.frequency_threshold)
+    interactions = tf.where(condition)
+    if interactions.shape[0] > 0:
+        index_kp_vec = tf.cast(interactions[:, 0], dtype=tf.int32)
+        # kp_vec = tf.unravel_index(index_kp_vec, phonons.kpts)
+        # kpp_vec = k[:, tf.newaxis] + (int(is_plus) * 2 - 1) * tf.cast(kp_vec[:, :], dtype=tf.int32)
+        # kpp_vec = tf.math.floormod(kpp_vec, phonons.kpts[:, tf.newaxis])
+        # index_kpp_vec = tf.cast(phonons.kpts[2] * phonons.kpts[1] * kpp_vec[0, :] + phonons.kpts[2] * kpp_vec[1, :] + kpp_vec[2, :], dtype=tf.int32)
+        index_kpp_vec = tf.gather(index_kpp_full, index_kp_vec)
+        mup_vec = tf.cast(interactions[:, 1], dtype=tf.int32)
+        mupp_vec = tf.cast(interactions[:, 2], dtype=tf.int32)
+        coords_1 = tf.stack((index_kp_vec, mup_vec), axis=-1)
+        coords_2 = tf.stack((index_kpp_vec, mupp_vec), axis=-1)
+        if sigma_tf.shape != []:
+            coords_3 = tf.stack((index_kp_vec, mup_vec, mupp_vec), axis=-1)
+            sigma_tf = tf.gather_nd(sigma_tf, coords_3)
+        if is_plus:
+            dirac_delta_tf = tf.gather_nd(phonons._occupations_tf, coords_1) - tf.gather_nd(phonons._occupations_tf, coords_2)
+
+        else:
+            dirac_delta_tf = 0.5 * (1 + tf.gather_nd(phonons._occupations_tf, coords_1) + tf.gather_nd(phonons._occupations_tf, coords_2))
+        dirac_delta_tf /= (tf.gather_nd(phonons._omega_tf, coords_1) * tf.gather_nd(phonons._omega_tf, coords_2))
+        dirac_delta_tf = dirac_delta_tf * 1 / tf.sqrt(np.pi * (2 * np.pi * sigma_tf) ** 2) * tf.exp(
+            - (phonons._omega_tf[index_k, mu] + second_sign * tf.gather_nd(phonons._omega_tf, coords_1) - tf.gather_nd(phonons._omega_tf, coords_2)) ** 2 / ((2 * np.pi * sigma_tf) ** 2))
+        index_kp = index_kp_vec
+        mup = mup_vec
+        index_kpp = index_kpp_vec
+        mupp = mupp_vec
+        return dirac_delta_tf, index_kp, mup, index_kpp, mupp
+
+def calculate_dirac_delta_amorphous(phonons, mu):
+    if phonons.frequencies[0, mu] < phonons.frequency_threshold:
+        return None
+    if phonons.broadening_shape == 'triangle':
+        delta_threshold = 1
+    else:
+        delta_threshold = DELTA_THRESHOLD
+    density_tf = phonons._occupations_tf
+    omega_tf = phonons._omega_tf
+    sigma_tf = tf.constant(phonons.sigma_in, dtype=tf.float64)
+    for is_plus in (1, 0):
+        second_sign = (int(is_plus) * 2 - 1)
+        omegas_difference = np.abs(phonons._omegas[0, mu] + second_sign * phonons._omegas[0, :, np.newaxis] -
+                                   phonons._omegas[0, np.newaxis, :])
+        condition = (omegas_difference < delta_threshold * 2 * np.pi * sigma_tf) & \
+                    (phonons.frequencies[0, :, np.newaxis] > phonons.frequency_threshold) & \
+                    (phonons.frequencies[0, np.newaxis, :] > phonons.frequency_threshold)
+        interactions = tf.where(condition)
+        if interactions.shape[0] > 0:
+            # Create sparse index
+            mup_vec = interactions[:, 0]
+            mupp_vec = interactions[:, 1]
+            if is_plus:
+                dirac_delta_tf = tf.gather(density_tf[0], mup_vec) - tf.gather(density_tf[0], mupp_vec)
+            else:
+                dirac_delta_tf = 0.5 * (1 + tf.gather(density_tf[0], mup_vec) + tf.gather(density_tf[0], mupp_vec))
+            dirac_delta_tf = dirac_delta_tf / tf.gather(omega_tf[0], mup_vec) / tf.gather(omega_tf[0], mupp_vec)
+            omegas_difference_tf = tf.abs(phonons._omegas[0, mu] + second_sign * tf.gather(omega_tf[0], mup_vec) - tf.gather(omega_tf[0],
+                                                                                                                mupp_vec))
+
+            if phonons.broadening_shape == 'gauss':
+                dirac_delta_tf = dirac_delta_tf * 1 / tf.sqrt(np.pi * (2 * np.pi * sigma_tf) ** 2) * np.exp(
+                    - omegas_difference_tf ** 2 / ((2 * np.pi * sigma_tf) ** 2))
+            elif phonons.broadening_shape == 'triangle':
+                dirac_delta_tf = dirac_delta_tf * 1. / (2 * np.pi * sigma_tf) * (1 - omegas_difference_tf / (2 * np.pi * sigma_tf))
+            else:
+                raise TypeError('Broadening function not implemented.')
+
+            try:
+                mup = tf.concat([mup, mup_vec], 0)
+                mupp = tf.concat([mupp, mupp_vec], 0)
+                current_delta = tf.concat([current_delta, dirac_delta_tf], 0)
+            except NameError:
+                mup = mup_vec
+                mupp = mupp_vec
+                current_delta = dirac_delta_tf
+    try:
+        return current_delta, mup, mupp
+    except:
+        return None
 
 
 def calculate_broadening(phonons, index_kpp_vec):
