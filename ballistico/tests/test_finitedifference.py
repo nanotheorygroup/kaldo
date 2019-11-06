32a68c312d305de989fc8bab679a226677111fd9
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-11-05 19:09:22 -0800

    Fix test to old finitedifference

--- ballistico/tests/test_finitedifference.py
+++ ballistico/tests/test_finitedifference.py
@@ -10,9 +10,8 @@ import numpy as np
 
 def test_max_freq_imported_dlpoly():
     supercell = np.array([3, 3, 3])
-    finite_difference = FiniteDifference.import_from_dlpoly_folder('finitedifference/tests/si-crystal', supercell)
+    finite_difference = FiniteDifference.import_from_dlpoly_folder('ballistico/tests/si-crystal', supercell)
     second_order = finite_difference.second_order / (28)
     eigv = np.linalg.eigh(second_order.reshape(162, 162))[0]
     freqs = np.sqrt(np.abs(eigv) * 9648.5) / (2 * np.pi)
     np.testing.assert_approx_equal(freqs.max(), 16.1, significant=3)
-
