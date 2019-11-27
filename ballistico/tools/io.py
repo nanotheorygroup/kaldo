f6d85f02dffc4fdf8d71aea7bda9f556823ad607
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-11-26 19:15:36 -0800

    Revert "Improve reading of third order to store only unit cell and not replicas"
    This reverts commit 6b932b9af2d01e497b5fa67fdef3b349f88922d6.

--- ballistico/tools/io.py
+++ ballistico/tools/io.py
@@ -37,32 +37,25 @@ def count_rows(filename):
     bufgen = takewhile(lambda x: x, (f.raw.read(1024 * 1024) for _ in repeat(None)))
     return sum(buf.count(b'\n') for buf in bufgen if buf)
 
-
 def import_sparse_third(atoms, replicas=(1, 1, 1), filename='THIRD'):
     n_inter = count_rows(filename)
     coords = np.zeros((n_inter * 3, 6), dtype=np.int16)
     values = np.zeros((n_inter * 3))
     replicas = np.array(replicas)
     n_replicas = np.prod(replicas)
-    n_unit_cell = atoms.get_positions().shape[0]
-    n_particles = n_unit_cell * n_replicas
+    n_particles = atoms.get_positions().shape[0] * n_replicas
     with open(filename) as f:
         for i, line in enumerate(f):
             l_split = re.split('\s+', line.strip())
-            coords_to_write = np.array(l_split[0:-3], dtype=int) - 1
-            if coords_to_write[0] >= n_unit_cell:
-                break
-            coords[3 * i:3 * (i + 1), :-1] = coords_to_write[np.newaxis, :]
+            coords[3 * i:3 * (i + 1), :-1] = (np.array(l_split[0:-3], dtype=int) - 1)[np.newaxis, :]
             coords[3 * i:3 * (i + 1), -1] = [0, 1, 2]
             values[3 * i:3 * (i + 1)] = np.array(l_split[-3:], dtype=np.float) * tenjovermoltoev
             if i % 1000000 == 0:
                 print('reading third order: ', np.round(i / n_inter, 2) * 100, '%')
-    print('read', 3 * i, 'interactions')
-    coords = coords[:3 * i].T
-    values = values[:3 * i]
-    sparse_third = COO (coords, values, shape=(n_unit_cell, 3, n_particles, 3, n_particles, 3))
+    print('read', i, 'interactions')
+    coords = coords.T
+    sparse_third = COO (coords, values, shape=(n_particles, 3, n_particles, 3, n_particles, 3))
     return sparse_third
-
 
 def import_dense_third(atoms, replicas, filename, is_reduced=True):
     replicas = np.array(replicas)
