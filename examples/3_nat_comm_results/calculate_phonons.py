b2f57e09f49a04464c9693d7b34b6ddfc3189f9b
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2020-03-13 12:17:35 -0700

    Update example

--- examples/3_nat_comm_results/calculate_phonons.py
+++ examples/3_nat_comm_results/calculate_phonons.py
@@ -7,10 +7,9 @@ from ballistico.conductivity import Conductivity
 from sklearn.neighbors.kde import KernelDensity
 from ballistico.controllers.plotter import plot_dispersion
 
-folder = '.'
-
 # Uncomment the following to import from lammps
-# folder = 'structure-lammps'
+
+# folder = 'structure_lammps'
 # config_file = str(folder) + '/replicated_coords.lmp'
 # dynmat_file = str(folder) + '/dynmat.dat'
 # third_file = str(folder) + '/third.dat'
@@ -22,24 +21,38 @@ folder = '.'
 #
 # finite_difference = FiniteDifference.from_files(replicated_atoms=atoms, dynmat_file=dynmat_file, folder=folder)
 
-finite_difference = FiniteDifference.from_folder('structure-dlpoly', format='eskm')
+finite_difference = FiniteDifference.from_folder('structure_dlpoly', format='eskm')
 
-phonons = Phonons (finite_difference=finite_difference,
-                   is_classic=False,
-                   temperature=300,
-                   # third_bandwidth=1/4.135,
-                   broadening_shape='triangle',
-                   is_tf_backend=True,
-                   folder='ald-output')
+# Conductivity AF
+phonons = Phonons(finite_difference=finite_difference,
+                  is_classic=False,
+                  temperature=300,
+                  # 1 THz
+                  # third_bandwidth=1/4.135,
+                  broadening_shape='triangle',
+                  is_tf_backend=True,
+                  folder='ald-output')
 
-delta = 0.5
+# Unfortunately at the moment the input is in 2 * pi * THZ
+# 0.5 THz
+phonons.diffusivity_bandwidth = 0.5 * 2 * np.pi / 4.135
+cond = Conductivity(phonons=phonons, method='qhgk').conductivity.sum(axis=0)
+cond = np.abs(np.mean(cond.diagonal()))
+print('AF conductivity: ', cond)
 
-# Note the 2 pi here. We are about to update that in a next version
-phonons.diffusivity_bandwidth = delta * (2 * np.pi / 4.135)
 
-# phonons.diffusivity_bandwidth = phonons.bandwidth
-print('AF conductivity')
+
+# Conductivity QHGK
+phonons = Phonons(finite_difference=finite_difference,
+                  is_classic=False,
+                  temperature=300,
+                  # 1 THz
+                  third_bandwidth=1/4.135,
+                  broadening_shape='triangle',
+                  is_tf_backend=True,
+                  folder='ald-output')
+
 
 cond = Conductivity(phonons=phonons, method='qhgk').conductivity.sum(axis=0)
 cond = np.abs(np.mean(cond.diagonal()))
-print(cond)
+print('QHGK conductivity: ', cond)
