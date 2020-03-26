710e04220976ced5cdc396188692d1fc177cb925
ZKC19940412 <zkuchen@ucdavis.edu>
2020-03-26 01:25:00 -0700

    created examples for hiphive-ballistico integration for si-bulk

---
+++ examples/4_silicon_bulk_hiphive/run_all.sh
@@ -0,0 +1,6 @@
+# Please cd to the example folder before you run this bash script
+
+python 1_prepare_reference_structures.py
+python 2_build_structure_container.py
+python 3_compute_fcs.py
+python 4_calculate_phonons.py
