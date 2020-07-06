b3fcf008a1b003f695738717995803b45b98d59e
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2020-07-06 14:04:04 -0700

    Update pandoc installation

---
+++ docs/install_pandoc.py
@@ -0,0 +1,5 @@
+# expects an installed pypandoc: pip install pypandoc
+from pypandoc.pandoc_download import download_pandoc
+# see the documentation how to customize the installation path
+# but be aware that you then need to include it in the `PATH`
+download_pandoc()
