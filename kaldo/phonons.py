Move reciprocal space calculation into second order Draft fix qe reader Add second_order q points

kaldo/phonons.py
@@ -7,9 +7,8 @@ from kaldo.helpers.storage import is_calculated
 from kaldo.helpers.storage import lazy_property
 from kaldo.helpers.storage import DEFAULT_STORE_FORMATS
 from kaldo.grid import Grid
-from kaldo.controllers.harmonic import calculate_physical_modes, calculate_frequency, calculate_velocity, \
-    calculate_heat_capacity, calculate_population, calculate_dynmat_derivatives, calculate_eigensystem, \
-    calculate_velocity_af, calculate_sij, calculate_sij_sparse, calculate_generalized_diffusivity, chi
+from kaldo.controllers.harmonic import calculate_physical_modes, \
+    calculate_heat_capacity, calculate_population, calculate_sij_sparse, calculate_generalized_diffusivity
 import numpy as np
 from opt_einsum import contract
 
@@ -135,7 +134,8 @@ class Phonons:
         frequency : np array
             (n_k_points, n_modes) frequency in THz
         """
-        frequency = calculate_frequency(self)
+        q_points = self._main_q_mesh
+        frequency = self.forceconstants.second_order.calculate_frequency(q_points)
         return frequency.reshape(self.n_k_points, self.n_modes)
 
 
@@ -147,7 +147,12 @@ class Phonons:
         velocity : np array
             (n_k_points, n_unit_cell * 3, 3) velocity in 100m/s or A/ps
         """
-        velocity = calculate_velocity(self)
+
+        q_points = self._main_q_mesh
+        velocity = self.forceconstants.second_order.calculate_velocity(q_points,
+                                                                       is_amorphous=self._is_amorphous,
+                                                                       distance_threshold=
+                                                                       self.forceconstants.distance_threshold)
         return velocity
 
 
@@ -249,7 +254,11 @@ class Phonons:
         -------
         flux : np.array(n_k_points, n_modes, n_k_points, n_modes, 3)
         """
-        sij = calculate_sij(self)
+        q_points = self._main_q_mesh
+        sij = self.forceconstants.second_order.calculate_sij(q_points,
+                                                             is_amorphous=self._is_amorphous,
+                                                             distance_threshold=
+                                                             self.forceconstants.distance_threshold)
         return sij
 
 
@@ -294,7 +303,8 @@ class Phonons:
 
     @lazy_property(label='')
     def _dynmat_derivatives(self):
-        dynmat_derivatives = calculate_dynmat_derivatives(self)
+        q_points = self._main_q_mesh
+        dynmat_derivatives = self.forceconstants.second_order.calculate_dynmat_derivatives(q_points)
         return dynmat_derivatives
 
 
@@ -310,7 +320,12 @@ class Phonons:
 
             If the system is not amorphous, these values are stored as complex numbers.
         """
-        eigensystem = calculate_eigensystem(self)
+        q_points = self._main_q_mesh
+
+        eigensystem = self.forceconstants.second_order.calculate_eigensystem(q_points=q_points,
+                                                                             is_amorphous=self._is_amorphous,
+                                                                             distance_threshold=
+                                                                             self.forceconstants.distance_threshold)
         return eigensystem
 
 
@@ -350,7 +365,11 @@ class Phonons:
 
     @property
     def _velocity_af(self):
-        velocity_AF = calculate_velocity_af(self)
+        q_points = self._main_q_mesh
+        velocity_AF = self.forceconstants.second_order.calculate_velocity_af(q_points,
+                                                                             is_amorphous=self._is_amorphous,
+                                                                             distance_threshold=
+                                                                             self.forceconstants.distance_threshold)
         return velocity_AF
 
 

kaldo/secondorder.py
@@ -7,10 +7,16 @@ import numpy as np
 from kaldo.interface.eskm_io import import_from_files
 import kaldo.interface.shengbte_io as shengbte_io
 import ase.units as units
+from kaldo.helpers.tools import timeit
+from kaldo.grid import wrap_coordinates
+from scipy.linalg.lapack import dsyev
+from kaldo.forceconstant import chi
 from kaldo.helpers.logger import get_logger
 logging = get_logger()
 
 SECOND_ORDER_FILE = 'second.npy'
+
+
 
 
 def acoustic_sum_rule(dynmat):
@@ -29,6 +35,7 @@ class SecondOrder(ForceConstant):
         if is_acoustic_sum:
             force_constant = acoustic_sum_rule(force_constant)
         ForceConstant.__init__(self, atoms, replicated_positions, supercell, force_constant)
+        self.n_modes = self.atoms.positions.shape[0] * 3
         self._list_of_replicas = None
 
 
@@ -45,7 +52,6 @@ class SecondOrder(ForceConstant):
         dynmat = contract('mialjb,i,j->mialjb', dynmat, 1 / np.sqrt(mass), 1 / np.sqrt(mass))
         evtotenjovermol = units.mol / (10 * units.J)
         return dynmat * evtotenjovermol
-
 
 
     @classmethod
@@ -164,6 +170,158 @@ class SecondOrder(ForceConstant):
         else:
             raise ValueError
         return second_order
+
+
+    @timeit
+    def calculate_frequency(self, q_points, is_amorphous=False, distance_threshold=None):
+        eigenvals = self.calculate_eigensystem(q_points, is_amorphous, distance_threshold, only_eigenvals=True)
+        frequency = np.abs(eigenvals) ** .5 * np.sign(eigenvals) / (np.pi * 2.)
+        return frequency.real
+
+
+    def calculate_dynmat_derivatives(self, q_points, is_amorphous=False, distance_threshold=None):
+        atoms = self.atoms
+        list_of_replicas = self.list_of_replicas
+        replicated_cell = self.replicated_atoms.cell
+        replicated_cell_inv = np.linalg.inv(self.replicated_atoms.cell)
+
+        dynmat = self.dynmat(atoms.get_masses())
+        positions = self.atoms.positions
+        n_unit_cell = atoms.positions.shape[0]
+        n_modes = n_unit_cell * 3
+        n_k_points = q_points.shape[0]
+        n_replicas = np.prod(self.supercell)
+
+        if distance_threshold is not None:
+            logging.info('Using folded flux operators')
+        cell_inv = np.linalg.inv(self.atoms.cell)
+
+        ddyn = np.zeros((n_k_points, n_unit_cell * 3, n_unit_cell * 3, 3)).astype(np.complex)
+        for index_k in range(n_k_points):
+            qvec = q_points[index_k]
+            if is_amorphous:
+                distance = positions[:, np.newaxis, :] - positions[np.newaxis, :, :]
+                distance = wrap_coordinates(distance, replicated_cell, replicated_cell_inv)
+                dynmat_derivatives = contract('ija,ibjc->ibjca', distance, dynmat[0, :, :, 0, :, :])
+            else:
+                distance = positions[:, np.newaxis, np.newaxis, :] - (
+                        positions[np.newaxis, np.newaxis, :, :] + list_of_replicas[np.newaxis, :, np.newaxis, :])
+
+                distance_to_wrap = positions[:, np.newaxis, np.newaxis, :] - (
+                    self.replicated_atoms.positions.reshape(n_replicas, n_unit_cell, 3)[
+                    np.newaxis, :, :, :])
+
+                list_of_replicas = self.list_of_replicas
+
+                if distance_threshold is not None:
+                    dynmat_derivatives = np.zeros((n_unit_cell, 3, n_unit_cell, 3, 3), dtype=np.complex)
+                    for l in range(n_replicas):
+                        wrapped_distance = wrap_coordinates(distance_to_wrap[:, l, :, :], replicated_cell,
+                                                            replicated_cell_inv)
+                        mask = (np.linalg.norm(wrapped_distance, axis=-1) < distance_threshold)
+                        id_i, id_j = np.argwhere(mask).T
+                        dynmat_derivatives[id_i, :, id_j, :, :] += np.einsum('fa,fbc->fbca', distance[id_i, l, id_j, :], \
+                                                                             dynmat[0, id_i, :, 0, id_j, :] *
+                                                                             chi(qvec, list_of_replicas, cell_inv)[l])
+                else:
+
+                    dynmat_derivatives = contract('ilja,ibljc,l->ibjca', distance, dynmat[0],
+                                                  chi(qvec, list_of_replicas, cell_inv))
+            ddyn[index_k] = dynmat_derivatives.reshape((n_modes, n_modes, 3))
+        return ddyn
+
+
+    def calculate_sij(self, q_points, is_amorphous=False, distance_threshold=None):
+        dynmat_derivatives = self.calculate_dynmat_derivatives(q_points, is_amorphous, distance_threshold)
+        eigenvects = self.calculate_eigensystem(q_points, is_amorphous, distance_threshold, only_eigenvals=False)[:, 1:, :]
+
+        logging.info('Calculating the flux operators')
+        if is_amorphous:
+            sij = np.tensordot(eigenvects[0], dynmat_derivatives[0], (0, 1))
+            sij = np.tensordot(eigenvects[0], sij, (0, 1))
+            sij = sij.reshape((1, sij.shape[0], sij.shape[1], sij.shape[2]))
+        else:
+            sij = contract('kim,kija,kjn->kmna', eigenvects.conj(), dynmat_derivatives, eigenvects)
+        return sij
+
+
+    def calculate_velocity_af(self, q_points, is_amorphous=False, distance_threshold=None):
+        n_modes = self.n_modes
+        sij = self.calculate_sij(q_points, is_amorphous, distance_threshold)
+        frequency = self.calculate_frequency(q_points, is_amorphous, distance_threshold)
+        sij = sij.reshape((q_points.shape[0], n_modes, n_modes, 3))
+        velocity_AF = contract('kmna,kmn->kmna', sij,
+                               1 / (2 * np.pi * np.sqrt(frequency[:, :, np.newaxis]) * np.sqrt(
+                                   frequency[:, np.newaxis, :]))) / 2
+        return velocity_AF
+
+
+    def calculate_velocity(self, q_points, is_amorphous=False, distance_threshold=None):
+        velocity_AF = self.calculate_velocity_af(q_points, is_amorphous, distance_threshold)
+        velocity = 1j * contract('kmma->kma', velocity_AF)
+        return velocity.real
+
+
+    def calculate_eigensystem(self, q_points, is_amorphous=False, distance_threshold=None, only_eigenvals=False):
+        atoms = self.atoms
+        n_unit_cell = atoms.positions.shape[0]
+        n_k_points = q_points.shape[0]
+        n_replicas = np.prod(self.supercell)
+        if distance_threshold is not None:
+            logging.info('Using folded dynamical matrix.')
+        if is_amorphous:
+            dtype = np.float
+        else:
+            dtype = np.complex
+        if only_eigenvals:
+            esystem = np.zeros((n_k_points, n_unit_cell * 3), dtype=dtype)
+        else:
+            esystem = np.zeros((n_k_points, n_unit_cell * 3 + 1, n_unit_cell * 3), dtype=dtype)
+        cell_inv = np.linalg.inv(self.atoms.cell)
+        replicated_cell_inv = np.linalg.inv(self.replicated_atoms.cell)
+
+        for index_k in range(n_k_points):
+            qvec = q_points[index_k]
+            dynmat = self.dynmat(atoms.get_masses())
+            is_at_gamma = (qvec == (0, 0, 0)).all()
+
+            list_of_replicas = self.list_of_replicas
+            if distance_threshold is not None:
+                dyn_s = np.zeros((n_unit_cell, 3, n_unit_cell, 3), dtype=np.complex)
+                replicated_cell = self.replicated_atoms.cell
+
+                for l in range(n_replicas):
+                    distance_to_wrap = atoms.positions[:, np.newaxis, :] - (
+                        self.replicated_atoms.positions.reshape(n_replicas, n_unit_cell,
+                                                                                               3)[np.newaxis, l, :, :])
+
+                    distance_to_wrap = wrap_coordinates(distance_to_wrap, replicated_cell, replicated_cell_inv)
+
+                    mask = np.linalg.norm(distance_to_wrap, axis=-1) < distance_threshold
+                    id_i, id_j = np.argwhere(mask).T
+
+                    dyn_s[id_i, :, id_j, :] += dynmat[0, id_i, :, 0, id_j, :] * chi(qvec, list_of_replicas, cell_inv)[l]
+            else:
+                if is_at_gamma:
+                    dyn_s = contract('ialjb->iajb', dynmat[0])
+                else:
+                    dyn_s = contract('ialjb,l->iajb', dynmat[0], chi(qvec, list_of_replicas, cell_inv))
+            dyn_s = dyn_s.reshape((self.n_modes, self.n_modes))
+
+
+            if only_eigenvals:
+                evals = np.linalg.eigvalsh(dyn_s)
+                esystem[index_k] = evals
+            else:
+                if is_at_gamma:
+                    evals, evects = dsyev(dyn_s)[:2]
+                else:
+                    evals, evects = np.linalg.eigh(dyn_s)
+                    # evals, evects = zheev(dyn_s)[:2]
+                esystem[index_k] = np.vstack((evals, evects))
+        return esystem
+
+
 
 
     def __str__(self):