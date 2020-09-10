645154603c8a4f7ba799e857201b004e7ab967a4
zkunchen <zkuchen@ucdavis.edu>
2020-09-09 18:29:56 -0700

    Update get_reference.sh

--- examples/silicon_clathrate_Tersoff_LAMMPS/get_reference.sh
+++ examples/silicon_clathrate_Tersoff_LAMMPS/get_reference.sh
@@ -2,7 +2,7 @@
 # obtain reference calculations
 # for example 4
 
-# Fetch precalculated forceconstant folder from remote
+# Fetch precalculated force constants folder from remote
 wget http://sophe.ucdavis.edu/structures/reference/reference_4_silicon_clathrate_Tersoff_LAMMPS.tar.gz
 # Untar precalculated files and clean up 
 tar xzvf reference_4_silicon_clathrate_Tersoff_LAMMPS.tar.gz
