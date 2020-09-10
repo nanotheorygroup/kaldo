df1e6d3a4c7b65e1acb61037c3342965f51c9467
zkunchen <zkuchen@ucdavis.edu>
2020-09-09 18:22:20 -0700

    Update get_precalculated_fc.sh

--- examples/silicon_bulk_LDA_QE_hiPhive/get_precalculated_fc.sh
+++ examples/silicon_bulk_LDA_QE_hiPhive/get_precalculated_fc.sh
@@ -1,8 +1,8 @@
 # get_precalculated_fc.sh is used to 
-# obtain precalculated forceconstant folder
+# obtain precalculated force constants folder
 #  for silicon bulk,using hiphive.
 
-# Fetch precalculated forceconstant folder from remote
+# Fetch precalculated force constants folder from remote
 wget http://sophe.ucdavis.edu/structures/hiphive_si_bulk.tar.gz
 # Untar precalculated files and clean up 
 tar xzvf hiphive_si_bulk.tar.gz
