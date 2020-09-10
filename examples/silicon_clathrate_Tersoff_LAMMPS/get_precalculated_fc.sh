612a62cc707559363f0901ea463b936ce416c654
zkunchen <zkuchen@ucdavis.edu>
2020-09-09 18:29:32 -0700

    Update get_precalculated_fc.sh

--- examples/silicon_clathrate_Tersoff_LAMMPS/get_precalculated_fc.sh
+++ examples/silicon_clathrate_Tersoff_LAMMPS/get_precalculated_fc.sh
@@ -1,11 +1,11 @@
 # get_precalculated_fc.sh is used to 
-# obtain precalculated forceconstant folder
+# obtain precalculated force constants folder
 #  for type 1 clathrate sample.
 
 # Clean up existed fc folder
 rm -rf fc_Si46
 
-# Fetch precalculated forceconstant folder from remote
+# Fetch precalculated force constants folder from remote
 wget http://sophe.ucdavis.edu/structures/fc_Si46.tar.gz
 
 # Untar precalculated files and clean up 
