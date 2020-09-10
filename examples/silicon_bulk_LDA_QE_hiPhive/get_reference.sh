b7d3dc42686fb1892c8e5540fd46e8897db44524
zkunchen <zkuchen@ucdavis.edu>
2020-09-09 18:22:38 -0700

    Update get_reference.sh

--- examples/silicon_bulk_LDA_QE_hiPhive/get_reference.sh
+++ examples/silicon_bulk_LDA_QE_hiPhive/get_reference.sh
@@ -2,7 +2,7 @@
 # obtain reference calculations
 # for example 3
 
-# Fetch precalculated forceconstant folder from remote
+# Fetch precalculated force constants folder from remote
 wget http://sophe.ucdavis.edu/structures/reference/reference_3_silicon_bulk_LDA_QE_hiPhive.tar.gz
 # Untar precalculated files and clean up 
 tar xzvf reference_3_silicon_bulk_LDA_QE_hiPhive.tar.gz
