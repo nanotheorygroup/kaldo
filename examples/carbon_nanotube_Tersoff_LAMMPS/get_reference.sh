d87c00b3d08e5c3f43da6b149f161df53f094529
zkunchen <zkuchen@ucdavis.edu>
2021-01-17 15:56:32 -0800

    Add files via upload

---
+++ examples/carbon_nanotube_Tersoff_LAMMPS/get_reference.sh
@@ -0,0 +1,11 @@
+# get_reference.sh is used to 
+# obtain reference calculations
+# for 10,0 carbon nanotube sample.
+
+# Fetch precalculated force constants folder from remote
+wget http://sophe.ucdavis.edu/structures/reference/updated_example/carbon_nanotube_Tersoff_LAMMPS.tar.gz
+# Untar precalculated files and clean up 
+tar xzvf carbon_nanotube_Tersoff_LAMMPS.tar.gz
+rm -rf carbon_nanotube_Tersoff_LAMMPS.tar.gz
+echo  "  "
+echo "Reference calculation files are obtained."
