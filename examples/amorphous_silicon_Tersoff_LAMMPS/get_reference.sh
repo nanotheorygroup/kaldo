ecdfe122ffbf88f68c62bd28f9d4701c9b9e8c44
zkunchen <zkuchen@ucdavis.edu>
2021-03-03 00:56:23 -0800

    Update get_reference.sh

--- examples/amorphous_silicon_Tersoff_LAMMPS/get_reference.sh
+++ examples/amorphous_silicon_Tersoff_LAMMPS/get_reference.sh
@@ -6,6 +6,7 @@
 # Fetch precalculated forceconstant folder from remote
 wget https://www.dropbox.com/s/8lvs173z8tjlw0m/amorphous_silicon_Tersoff_LAMMPS.tar.gz?dl=0
 mv amorphous_silicon_Tersoff_LAMMPS.tar.gz?dl=0 amorphous_silicon_Tersoff_LAMMPS.tar.gz
+
 # Untar precalculated files and clean up 
 tar xzvf amorphous_silicon_Tersoff_LAMMPS.tar.gz
 rm -rf amorphous_silicon_Tersoff_LAMMPS.tar.gz
