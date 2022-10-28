a77d9bbb97e402d7d2732337ea0e0773834631f2
Davide Donadio <ddonadio@ucdavis.edu>
2022-10-28 14:34:38 -0700

    Update displacement.py

--- kaldo/controllers/displacement.py
+++ kaldo/controllers/displacement.py
@@ -86,13 +86,12 @@ def calculate_third(atoms, replicated_atoms, third_order_delta, distance_thresho
     n_forces_to_calculate = n_replicas * (n_atoms * 3) ** 2
     n_forces_done = 0
     n_forces_skipped = 0
-    np.savetxt('replicatedpos.dat', replicated_atoms.positions)
     for iat in range(n_atoms):
         for jat in range(n_replicas * n_atoms):
             is_computing = True
             m, j_small = np.unravel_index(jat, (n_replicas, n_atoms))
             if (distance_threshold is not None):
-                dxij = atoms.positions[iat] - (replicated_atoms.positions[m]) # + atoms.positions[j_small]) 
+                dxij = atoms.positions[iat] - replicated_atoms.positions[jat] 
                 if (np.linalg.norm(dxij) > distance_threshold):
                     is_computing = False
                     n_forces_skipped += 9
