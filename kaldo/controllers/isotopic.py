2753b888cc877c540a535e310ed3cb2f5e81e825
alfredo fiorentino <afiorent@alfredos-MBP.lan>
2024-03-12 16:53:43 +0100

    update isotopic_bw adaptive gaussian

--- kaldo/controllers/isotopic.py
+++ kaldo/controllers/isotopic.py
@@ -11,7 +11,7 @@ from kaldo.controllers.dirac_kernel import gaussian_delta, triangular_delta, lor
 logging = get_logger()
 
 @timeit
-def compute_isotopic_bw(phonons,sigma=0.5):
+def compute_isotopic_bw(phonons):
     #TODO: add criteria for sigma
     n_atoms=phonons.n_atoms
     n_modes = phonons.n_modes
@@ -21,6 +21,17 @@ def compute_isotopic_bw(phonons,sigma=0.5):
     omegas = phonons.omega
     eigvectors = phonons.eigenvectors
     eigvectors=eigvectors.reshape([n_k_points,n_atoms,3,n_modes])
+    if phonons.third_bandwidth:
+        sigmas = phonons.third_bandwidth*np.ones_like(omegas)
+    else:
+        # TODO compare to anharmonic compute broad function
+        # sigma = 0.3*2*np.pi
+        # logging.info(' default sigma={}'.format(sigma))
+        velocity=phonons.velocity
+        cellinv = phonons.forceconstants.cell_inv
+        k_size = phonons.kpts
+        sigmas = calculate_base_sigma(velocity, cellinv, k_size)
+        sigmas=refine_sigma(base_sigma=sigmas)
     if phonons.broadening_shape == 'lorentz':
         logging.info('Using Lorentzian diffusivity_shape')
         curve = lorentz_delta
@@ -38,12 +49,17 @@ def compute_isotopic_bw(phonons,sigma=0.5):
             logging.info('Calculating isotopic bandwidth  ' + str(nu_single) +  ', ' + \
                          str(np.round(nu_single / phonons.n_phonons, 2) * 100) + '%')
         index_k, mu = np.unravel_index(nu_single, (n_k_points, phonons.n_modes))
+        sigma=sigmas[index_k,mu]
+        # if phonons.third_bandwidth is None:
+        #     sigma = calculate_base_sigma(velocity, cellinv, k_size, index_k, mu)
+        #     sigma=np.mean(sigma)
         vec=eigvectors[index_k,:,:,mu]
+
         overlap=contract('kixn,ix->kin',eigvectors,np.conjugate(vec) )
         overlap=np.abs(overlap)**2
         g_per_mode=contract('kin,i->kn',overlap,g_factor)
         delta_omega=omegas-omegas[index_k,mu]
-        w2delta=omegas**2*curve(delta_omega,sigma)
+        w2delta=omegas**2*curve(delta_omega,2*np.pi*sigma)
         bw=w2delta*g_per_mode/n_k_points
         isotopic_bw[index_k,mu]=(np.pi/2)*np.sum(bw) #check prefactor
 
@@ -51,3 +67,33 @@ def compute_isotopic_bw(phonons,sigma=0.5):
 
 
     return isotopic_bw
+
+
+def calculate_base_sigma(velocity, cellinv, k_size):
+    #sigma: array (nk,nmodes)
+    #local adaptive broadening from Shengbte
+    # we want the last index of velocity (the coordinate index to dot from the right to rlattice vec
+    delta_k =np.dot( cellinv, 1/ k_size)
+    base_sigma = (contract('knx,x->kn',velocity,delta_k))**2
+    base_sigma = np.sqrt(base_sigma/6 )
+    return base_sigma
+
+def refine_sigma(base_sigma):
+    #sigma: array (nk,nmodes)
+    #local adaptive broadening from Shengbte
+    #avoid sigma too extreme ( e.g. zero)
+    sigma=base_sigma.copy()
+    sigma[base_sigma<=0]=np.min(sigma[base_sigma>0])
+    logsigma=np.log(sigma)
+    per25=np.percentile(logsigma, 25)
+    per50 = np.percentile(logsigma, 25)
+    per75 = np.percentile(logsigma, 75)
+    # delta=per75-per25
+    # lbound=np.exp(per25-1.5*delta)
+    lbound=np.exp(per50)
+    logging.info('per25,per75,lbound={} {} {}'.format(np.exp(per25),np.exp(per75),lbound))
+    # lbound=np.exp(np.percentile(logsigma, 50))
+    sigma=np.where(sigma>lbound,sigma,lbound)
+    # sigma=np.max(sigma)*np.ones_like(base_sigma)
+
+    return sigma
