0665550e4e9e21b0dcf1bbc4ec24d369bc930e4c
alfredo fiorentino <afiorent@alfredos-MacBook-Pro.local>
2024-05-06 10:17:58 +0200

    minor update test ge qe

--- kaldo/tests/test_ge_qe_crystal.py
+++ kaldo/tests/test_ge_qe_crystal.py
@@ -12,10 +12,10 @@ import pytest
 
 @pytest.yield_fixture(scope="session")
 def phonons():
-    print ("Preparing phonons object.")
+    print("Preparing phonons object.")
     forceconstants = ForceConstants.from_folder(folder='kaldo/tests/ge-crystal',
-                                                     supercell=[10, 10, 10],
-                                                     format='shengbte-d3q',
+                                                supercell=[10, 10, 10],
+                                                format='shengbte-d3q',
                                                 only_second=True)
     phonons = Phonons(forceconstants=forceconstants,
                       kpts=[7, 7, 7],
@@ -26,14 +26,11 @@ def phonons():
     return phonons
 
 
-
-
-
 def test_frequency(phonons):
-    freq=phonons.frequency.flatten()
+    freq = phonons.frequency.flatten()
     np.testing.assert_approx_equal(freq[3], 8.99, significant=3)
+
+
 def test_iso_bw(phonons):
-    freq=phonons.isotopic_bandwidth.flatten()
-    np.testing.assert_approx_equal(freq[3], 7.85*1e-3, significant=3)
-
-
+    freq = phonons.isotopic_bandwidth.flatten()
+    np.testing.assert_approx_equal(freq[3], 7.85 * 1e-3, significant=3)
