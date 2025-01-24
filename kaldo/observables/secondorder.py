c7c0c2af3c864f8d3fe5b17019c0414aafab1881
bkhli <194544471+bkhli@users.noreply.github.com>
2025-01-23 18:33:38 -0800

    fix vasp/shengbte read error

--- kaldo/observables/secondorder.py
+++ kaldo/observables/secondorder.py
@@ -367,7 +367,7 @@ class SecondOrder(ForceConstant):
                     raise FileNotFoundError(f"File {filename} not found.")
                 second_order = shengbte_io.read_second_order_matrix(folder, supercell)
                 second_order = second_order.reshape((n_unit_atoms, 3, n_replicas, n_unit_atoms, 3))
-                grid_type = "C"
+                grid_type = "F"
             second_order = SecondOrder.from_supercell(
                 atoms=atoms,
                 grid_type=grid_type,
