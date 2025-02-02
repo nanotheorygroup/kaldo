0c5fb4b5dc2335a3ce1bd7dc783071266232454d
bkhli <194544471+bkhli@users.noreply.github.com>
2025-02-01 17:32:45 -0800

    observable.py improves code readbility

--- kaldo/observables/observable.py
+++ kaldo/observables/observable.py
@@ -10,11 +10,9 @@ class Observable:
     def __init__(self, folder: str = FOLDER_NAME, *kargs, **kwargs):
         self.folder = folder
 
-
     @classmethod
     def load(cls, *kargs, **kwargs):
         pass
-
 
     def save(self, property_name: str | None = None, format: str = 'numpy'):
         folder = self.folder
@@ -22,25 +20,29 @@ class Observable:
         if property_name is None:
             property_name = str(self)
         name = folder + '/' + property_name
-        if format == 'numpy':
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            np.save(name + '.npy', loaded_attr)
-            logging.info(name + ' stored')
-        elif format == 'hdf5':
-            with h5py.File(name.split('/')[0] + '.hdf5', 'a') as storage:
-                if name not in storage:
-                    storage.create_dataset(name, data=loaded_attr, chunks=True, compression='gzip',
-                                           compression_opts=9)
-            logging.info(name + ' stored')
-        elif format == 'formatted':
-            # loaded_attr = np.nan_to_num(loaded_attr)
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            fmt = '%.18e'
-            np.savetxt(name + '.dat', loaded_attr, fmt=fmt, header=str(loaded_attr.shape))
-        elif format == 'memory':
-            logging.warning('Property ' + str(property_name) + ' will be lost when calculation is over.')
-        else:
-            raise ValueError('Storing format not implemented')
 
+        match format:
+            case 'numpy':
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                np.save(name + '.npy', loaded_attr)
+                logging.info(name + ' stored')
+
+            case 'hdf5':
+                with h5py.File(folder + '.hdf5', 'a') as storage:
+                    if name not in storage:
+                        storage.create_dataset(name, data=loaded_attr, chunks=True, compression='gzip',
+                                               compression_opts=9)
+                logging.info(name + ' stored')
+
+            case 'formatted':
+                # loaded_attr = np.nan_to_num(loaded_attr)
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                np.savetxt(name + '.dat', loaded_attr, fmt="%.18e", header=str(loaded_attr.shape))
+
+            case 'memory':
+                logging.warning('Property ' + str(property_name) + ' will be lost when calculation is over.')
+
+            case _:
+                raise ValueError('Storing format' + format + ' not implemented')
